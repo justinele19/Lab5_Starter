b363ea87cd47eae72b4b908f890d1c2259ce6d3f
Justine <lejustinek@gmail.com>
2026-05-04 23:53:41 -0700

    added explore.js and unit tests

--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -9,3 +9,77 @@ import {
 } from '../code-to-unit-test/unit-test-me';
 
 // TODO - Part 2
+
+// Test isPhoneNumber 
+test('this is a valid phone number (949) 239-3433', () => {
+    expect(isPhoneNumber('(949) 239-3433')).toBe(true);
+});
+test('this is a valid phone number 434-233-3445', () => {
+    expect(isPhoneNumber('434-233-3445')).toBe(true);
+});
+test('this is not a valid phone number 6769893408', () => {
+    expect(isPhoneNumber('6769893408')).toBe(false);
+});
+test('this is not a valid phone number heyyy', () => {
+    expect(isPhoneNumber('heyyy')).toBe(false);
+});
+
+
+// Test isEmail 
+test('this is a valid email hello@gmail.com', () => {
+    expect(isEmail('hello@gmail.com')).toBe(true);
+});
+test('this is a valid email boo@yahoo.edu', () => {
+    expect(isEmail('boo@yahoo.edu')).toBe(true);
+});
+test('this is not a valid email @gmail.com', () => {
+    expect(isEmail('@gmail.com')).toBe(false);
+});
+test('this is not a valid email .com@', () => {
+    expect(isEmail('.com@')).toBe(false);
+});
+
+
+// Test isStrongPassword 
+test('this is a valid strong password hello123', () => {
+    expect(isStrongPassword('hello123')).toBe(true);
+});
+test('this is a valid strong password YOO_421', () => {
+    expect(isStrongPassword('YOO_421')).toBe(true);
+});
+test('this is not a valid strong password hey', () => {
+    expect(isStrongPassword('hey')).toBe(false);
+});
+test('this is not a valid strong password 1922_hehe', () => {
+    expect(isStrongPassword('1922_hehe')).toBe(false);
+});
+
+
+// Test isDate 
+test('this is a valid date 2/19/2005', () => {
+    expect(isDate('2/19/2005')).toBe(true);
+});
+test('this is a valid date 02/22/1954', () => {
+    expect(isDate('02/22/1954')).toBe(true);
+});
+test('this is not a valid date 8 - 22 - 2006', () => {
+    expect(isDate('8 - 22 - 2006')).toBe(false);
+});
+test('this is not a valid date 8/5/72', () => {
+    expect(isDate('8/5/72')).toBe(false);
+});
+
+
+// Test isHexColor 
+test('#FFF is a valid hex color', () => {
+    expect(isHexColor('#FFF')).toBe(true);
+});
+test('#FF5733 is a valid hex color', () => {
+    expect(isHexColor('#FF5733')).toBe(true);
+});
+test('#GGGGGG is not a valid hex color', () => {
+    expect(isHexColor('#GGGGGG')).toBe(false);
+});
+test('123456789 is not a valid hex color', () => {
+    expect(isHexColor('123456789')).toBe(false);
+});
--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,49 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const faceImg = document.querySelector('#explore img');
+  const textBox = document.getElementById('text-to-speak');
+  const voiceSelect = document.getElementById('voice-select');
+  const talkButton = document.querySelector('button');
+  const synth = window.speechSynthesis;
+
+  function loadVoices() {
+    const voices = synth.getVoices();
+    voices.forEach((voice, index) => {
+      const option = document.createElement('option');
+      option.value = index;
+      option.textContent = `${voice.name} (${voice.lang})`;
+      voiceSelect.appendChild(option);
+    });
+  }
+
+  loadVoices();
+  synth.addEventListener('voiceschanged', loadVoices);
+
+  talkButton.addEventListener('click', () => {
+    const text = textBox.value.trim();
+    if (!text || voiceSelect.value === 'select') return;
+
+    synth.cancel();
+
+    // Built-in browser object that represents a speech request
+    const utter = new SpeechSynthesisUtterance(text);
+
+    // Set selected voice 
+    const voices = synth.getVoices();
+    utter.voice = voices[voiceSelect.value];
+
+    utter.onstart = () => {
+      faceImg.src = 'assets/images/smiling-open.png';
+      faceImg.alt = 'Open smiling mouth face';
+    }
+
+    utter.onend = () => {
+      faceImg.src = 'assets/images/smiling.png';
+      faceImg.alt = 'Smiling face';
+    };
+
+    synth.speak(utter);
+    
+  });
 }
